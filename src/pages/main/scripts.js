6684c890be6a78ccdbe3ff966c3675c2fab5e94c
abozhin777 <75569624+abozhin777@users.noreply.github.com>
2021-05-05 21:43:26 +0400

    На проверочку

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,18 +1,54 @@
-var popup = document.querySelector('.popup-contact');
-var overlay = document.querySelector('.popup-overlay');
-var popupBtn = document.querySelector('.contact-us');
-var popupCloseBtn = popup.querySelector('.popup-contact-btn');
-var input = popup.querySelector('input');
-
-popupBtn.addEventListener('click', function(){
-    popup.classList.add('popup-contact-open');
-    overlay.classList.add('show');
-    input.focus();
-
-    popupCloseBtn.addEventListener('click', function(){
-        popup.classList.remove('popup-contact-open');
-        overlay.classList.remove('show');
-    })  
+var popup = document.querySelector ('.popup')
+var overLay = document.querySelector ('.popup__overlay')
+var popupOpenBtn = document.querySelector ('.slider-offer__button')
+var popupCloseBtn = popup.querySelector ('.popup__close-button')
+var input = popup.querySelector ('input')
 
 
+popupOpenBtn.addEventListener ('click', function(){
+    popup.classList.add('open');
+    overLay.classList.add('show');
+    input.focus()
+    window.addEventListener('keydown', function(evt){
+        if (evt.code === "Escape") {
+            popup.classList.remove('open');
+            overLay.classList.remove('show');
+        }
+    })
+})
+popupCloseBtn.addEventListener('click', function(){
+    popup.classList.remove('open');
+    overLay.classList.remove('show');
+})
+
+var header = document.querySelector ('.header')
+var headerMenu = document.querySelector ('.header-mobile')
+var headerOverlay = document.querySelector ('.header-overlay')
+var headerOpenBtn = document.querySelector ('.header__menu-btn')
+var headerCloseBtn = header.querySelector ('.header__close-btn')
+var input = popup.querySelector ('input')
+
+
+headerOpenBtn.addEventListener ('click', function(){
+    header.classList.add('header-fixed');
+    headerMenu.classList.add('open');
+    headerOverlay.classList.add('open');
+    headerOpenBtn.classList.add('close');
+    headerCloseBtn.classList.add('open');
+    window.addEventListener('keydown', function(evt){
+        if (evt.code === "Escape") {
+            header.classList.remove('header-fixed');
+            headerMenu.classList.remove('open');
+            headerOverlay.classList.remove('open');
+            headerOpenBtn.classList.remove('close');
+            headerCloseBtn.classList.remove('open');
+        }
+    })
+})
+headerCloseBtn.addEventListener('click', function(){
+    header.classList.remove('header-fixed');
+    headerMenu.classList.remove('open');
+    headerOverlay.classList.remove('open');
+    headerOpenBtn.classList.remove('close');
+    headerCloseBtn.classList.remove('open');
 })
